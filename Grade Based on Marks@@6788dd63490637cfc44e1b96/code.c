caefa06d4d73e532860def9b24814fded5c7c24f
mansha1373 <mansha1373.becse24@chitkara.edu.in>
2025-01-30 11:36:45 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int mark;
+    scanf("%d", &mark);
+    if( mark>=90){
+        printf("A");
+    }
+    else if(mark>=80 && mark <90){
+        printf("B");
+    }
+    else if(mark>= 70 && mark<80){
+        printf("C");
+    }
+    else if(mark>=60 && mark<70){
+        pritnf("D");
+    }
+    else{
+        printf("F");
+    }
+    return 0;
+}
