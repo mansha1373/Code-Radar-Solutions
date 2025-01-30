82dac0726df38354aa81e43868914e0d7ea9b8ad
mansha1373 <mansha1373.becse24@chitkara.edu.in>
2025-01-30 19:25:27 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+    scanf("%c", &ch);
+    if(ch == 'A'){
+        printf("Excellent");
+    }
+    else if (ch == 'B'){
+        printf("Good");
+    }
+    else if (ch== 'C'){
+        printf("Average");
+    }
+    else if (ch== 'D'){
+        printf("Below Average");
+    }
+    else if (ch == 'F'){
+        printf("Fail");
+    }
+    return 0;
+}
